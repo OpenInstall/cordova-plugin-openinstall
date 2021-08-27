c15a34f7c3c2bed8058186e9497740da99595716
WuWenhui <946366658@qq.com>
2021-08-27 11:29:02 +0800

    升级Android SDK 到2.5.4；新增接口config和init

--- www/cordova-plugin-openinstall.js
+++ www/cordova-plugin-openinstall.js
@@ -2,9 +2,39 @@ var exec = require('cordova/exec');
 
 module.exports = {
     
+
+    /**
+     * 新添加api（android初始化前配置）
+     * @param options 配置参数：
+     * {
+     *      adEnabled: true, 
+     *      macDisabled: true,
+     *      imeiDisabled: true,
+     *      gaid: "通过 google api 获取到的 advertisingId",
+     *      oaid: "通过移动安全联盟获取到的 oaid"
+     * }
+     */
+    config: function(options){
+        function pass() {};
+        
+        exec(pass, pass, "OpenInstallPlugin", "config", [options.adEnabled, 
+            options.macDisabled, options.imeiDisabled, options.gaid, options.oaid]);
+    },
+
+    /**
+     * 新添加api（android 初始化调用）
+     * 初始化
+     */
+    init: function(){
+        function pass() {};
+        
+        exec(pass, pass, "OpenInstallPlugin", "init", []);
+    },
+
     /**
      * 获取安装参数
      * @param onSuccess 成功回调：数据格式为 {'channel': 1002, 'data': {'key': 'value'}}
+     * @param onError 错误回调：返回错误信息
      * @param time 超时时间：整形值，单位秒
      */
     getInstall: function (onSuccess, onError, time){
@@ -13,6 +43,8 @@ module.exports = {
 
     /**
      * 注册唤醒监听
+     * @param onSuccess 成功回调：数据格式为 {'channel': 1002, 'data': {'key': 'value'}}
+     * @param onError 错误回调：返回错误信息
      */
     registerWakeUpHandler: function(onSuccess, onError){
         exec(onSuccess, onError, "OpenInstallPlugin", "registerWakeUpHandler", []);
